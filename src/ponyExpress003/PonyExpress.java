a43fad4c155a27a1ea03422cbbfa779f138eb0b3
Virginia <virginiapiazza19@gmail.com>
2021-08-24 20:57:44 -0300

    Se modificó el 0

--- src/ponyExpress003/PonyExpress.java
+++ src/ponyExpress003/PonyExpress.java
@@ -3,12 +3,13 @@ package ponyExpress003;
 public class PonyExpress {
 	
 	private static final int MILLAS_MAXIMAS = 100;
+	private static final int MILLAS_MINIMAS = 0;
 	
 	public int caballos(int[] distancias) {
 		int cantidadDeCaballos = 1;
-		int millas = 0;
-		for (int distancia = 0; distancia<distancias.length; distancia++) {
-			if ((distancias[distancia] > MILLAS_MAXIMAS) || (distancias[distancia] < 0)) {
+		int millas = MILLAS_MINIMAS;
+		for (int distancia = MILLAS_MINIMAS; distancia<distancias.length; distancia++) {
+			if ((distancias[distancia] > MILLAS_MAXIMAS) || (distancias[distancia] < MILLAS_MINIMAS)) {
 				throw new Error("La distancia entre estación y estación debe estar entre 1 y 100 millas");
 			}  else
 			millas += distancias[distancia];
